4d06a10a98452248e1d8afcfeb886d4ce6736726
Kacy Rowe <>
2020-02-17 15:23:08 -0800

    big overhail, backend update

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField() ## unrestricted text because posts can be quite long
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE) ## (argument)if user gets deleted, delete their post
+
+    def __str__(self):
+        ## dunder str method (double underscore)
+        ## these are known as "magic methods"
+        return self.title
--- myRoomie/settings.py
+++ myRoomie/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
+    'users.apps.UsersConfig',
     'main.apps.mainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -119,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
