0da06adb1c0e2340e9e4797daf8d3076d8bf92e1
Kacy Rowe <>
2020-02-26 15:59:06 -0800

    steves updates temp

--- main/models.py
+++ main/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
@@ -9,6 +10,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE) ## (argument)if user gets deleted, delete their post
 
     def __str__(self):
-        ## dunder str method (double underscore)
-        ## these are known as "magic methods"
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Post #from models file in current package
-
+from users.models import Profile
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 # dummy data from the tutorial, ineffective way of doing this
 posts = [
@@ -26,9 +28,56 @@ def home(request):
     }
     return render(request, 'main/home.html', context)
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'main/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    #<app>/<model>_<viewtype>.html
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title','content']
+    success_url = '/' #use this line to return to main page after making an announcement
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title','content']
+    success_url = '/' #use this line to return to main page after making an announcement
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url='/'
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
 
 def about(request):
     return render(request, 'main/about.html', {'title': 'About'})
 
-def payments(request):
-    return render(request, 'main/payments.html', {'title': 'Payments'})
+def roommates(request):
+    context = {
+        # 'posts': posts
+        'profiles': Profile.objects.all()
+    }
+    return render(request, 'main/roommates.html', context)
