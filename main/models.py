9c531b9c3a2975bfbe838d92e1a8c1c02d9732d0
Kacy Rowe <>
2020-02-27 20:34:46 -0800

    Merge all, Claude and Steven work

--- main/models.py
+++ main/models.py
@@ -2,7 +2,10 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+# this communicates directly with the database and creates the necessary tables and attributes for us
+
 class Post(models.Model):
+    # announcements posts
     title = models.CharField(max_length=100)
     content = models.TextField() ## unrestricted text because posts can be quite long
     date_posted = models.DateTimeField(default=timezone.now)
@@ -11,4 +14,22 @@ class Post(models.Model):
     def __str__(self):
         ## dunder str method (double underscore)
         ## these are known as "magic methods"
+        return self.title
+
+
+class Bills(models.Model):
+    # billing and payments page
+    # making changes to this page requires migrations to the database!
+    title = models.CharField(max_length=100)
+    date_posted = models.DateTimeField(default=timezone.now)
+    amount = models.DecimalField(max_digits=6, decimal_places=2)
+    ispaid = models.BooleanField(default=False)
+    date_due = models.DateField()
+    bills_total = models.Sum()
+
+    def __str__(self):
+        ## dunder str method (double underscore)
+        ## these are known as "magic methods"
+
+        #cast the decimal value to a string to return
         return self.title
--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from . import views
 
 urlpatterns = [
     path('', views.home, name='main-home'),
+    path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
+    path('post/new', PostCreateView.as_view(), name='post-create'),
     path('about/', views.about, name='main-about'),
-    path('payments/', views.payments, name='main-payments')
+    path('bills/', views.bills, name='main-bills'),
+    path('roommates/', views.roommates, name='main-roommates'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
